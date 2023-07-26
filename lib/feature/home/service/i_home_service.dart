22d453f645f41ed2f36dc31dc01d1afd62a7e2a4
Hüseyin Gür <huseyingur.ceng@gmail.com>
2023-07-26 15:26:13 +0300

    updated project

--- lib/feature/home/service/i_home_service.dart
+++
@@ -1,5 +0,0 @@
-import 'package:weather_app/feature/home/model/weather_model.dart';
-
-abstract class IHomeService {
-  Future<WeatherModel> getWeather();
-}
--- lib/feature/home/viewmodel/home_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'home_state.dart';
-
-class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
-}
